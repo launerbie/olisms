c66bd432ddcd2ecb271106bec3a95cf243ac3024
iambernie <iambernie@gmail.com>
2014-07-30 22:55:12 +0200

    added few cmd args

--- autocorrelation.py
+++ autocorrelation.py
@@ -4,55 +4,123 @@ import os
 import h5py
 import numpy as np
 import numpy 
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib import colors
 import argparse
 import progressbar as pb
 
-def main():
-    """
-    Create 'figures' directory if it doesn't exist. Make plots and save plots 
-    to this directory.
-    """
-    from colors import rundark
-    rundark()
+# TODO: select only a subset of available Temperatures
+# TODO: seperate arguments for enery acf figure and magnetization acf figure
+# TODO: logscale
 
-    directory = 'figures'
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    f = h5py.File(args.filename) 
-    basename = os.path.basename(args.filename)
+def get_basename(filepath):
+    """ If filepath = 'some_path/myfile.hdf5', then this returns 'myfile'. """
+    basename = os.path.basename(filepath)
     name = os.path.splitext(basename)[0]
+    return name
 
-    make_combined_plots(f,directory,name)
-
-def acf(x, length):
+def acf(x, length=None):
     """
-    x: 1d array
-    length: maximum dt for which to calculate correlation
+    Return an ndarray with normalized correlation coefficients corresponding 
+    with lag elements given by: range(1, length).  
+    The range starts with 1 because the correlation for lag=0 is infinity.
 
-    See https://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
-    for the definition of the correlation matrix.
+    Parameters
+    ----------
+    x : ndarray of shape (1,)
+        The signal (typically time-series data) for which the autocorrelation 
+        function C(lag) needs to calculated.
+
+    length : int 
+        End of the interval range(1, length). The normalized correlation 
+        coefficients are calculated for this range and returned as an ndarray.
+        #TODO: or optional array of lag elements [dt1, dt2, dt3,...]
+
+    See:
+
+    https://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
+
+    for the definition of the correlation matrix. 
+
+    The correlation coefficients are returned by np.corrcoef. See for details:
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.corrcoef.html
+    
     """
+    if not length: 
+        length = len(x) - 1
+
     coeffs = [np.corrcoef(x[:-dt], x[dt:])[0,1] for dt in range(1,length)]
-    return numpy.array([1]+ coeffs)
+    result = numpy.array([1]+ coeffs)
+    return result
 
-def make_combined_plots(f, directory, name):
-    sims = f.values()
 
-    #cmap = cm.RdBu_r 
-    cmap = cm.hot 
+def make_acf_plot(h5pyfile, name, **kwargs):
+    """
+    Valid keyword arguments are:
+    h5path: 
+    norminterval:
+    cmap:
+    length:
+    xlim:
+    img_suffix:
+    targetdir:
+    """
+    #print("Keyword arguments in make_acf_plot:", kwargs)
 
-    firstsim = list(f.values())[0]
-    shape = firstsim['shape'][0]
-    algorithm = f.attrs['mode']
+    if 'h5path' not in kwargs:
+        print("Please specify timeseries data")
+        return 0
+    else:
+        h5path = kwargs['h5path']
+
+    # Set up figure parameters here
+    if 'norminterval' in kwargs:
+        norm = colors.Normalize(*kwargs['norminterval'])
+    else:
+        norm = colors.Normalize(vmin=1.4, vmax=3.6)
+
+    if 'cmap' in kwargs:
+        cmap = kwargs['cmap']
+    else:
+        cmap = cm.hot
+
+    if 'length' in kwargs:
+        length = kwargs['length']  
+    else:
+        length = None 
+
+    if 'xlim' in kwargs:
+        xlim = kwargs['xlim']
+    else:
+        xlim = None
+
+    if 'img_suffix' in kwargs:
+        img_suffix = kwargs['img_suffix']
+    else:
+        img_suffix = "_{}_acf".format(h5path)
+
+    if 'targetdir' in kwargs:
+        targetdir = kwargs['targetdir']
+    else:
+        targetdir = 'figures'
+
 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    
-    pbar = pb.ProgressBar(widgets=drawwidget("Generating ACF Magnetization: "),
+
+    sims = h5pyfile.values()
+    firstsim = list(h5pyfile.values())[0]
+    shape = firstsim['shape'].value
+    algorithm = h5pyfile.attrs['mode']
+
+    shape_as_LxL_string = str(shape[0][0])+" x "+str(shape[0][1]) # e.g. "20 x 20"
+
+
+    print("\nHDF5 file: {}".format(h5pyfile.filename))
+    pbar = pb.ProgressBar(widgets=drawwidget("Generating ACF {}: ".format(h5path)),
                   maxval=len(sims)).start()
     
     for simnr, sim in enumerate(sims):
@@ -60,57 +128,31 @@ def make_combined_plots(f, directory, name):
 
         T = sim['temperature'][0]
         #temperature based linecolor
-        norm = colors.Normalize(vmin=1.4,vmax=3.6)
         linecolor = cmap(norm(T))
 
-        magnetization = sim['magnetization']
+        timeseriesdata = sim[h5path]
 
-        ax1.plot(acf(magnetization, 500), c=linecolor)
+        ax1.plot(acf(timeseriesdata, length), c=linecolor)
 
-    ax1.set_xlabel('dt [in sweeps]')
-    ax1.set_ylabel('C (dt)')
-    ax1.set_xlim(0,100)
-    ax1.set_ylim(-0.1,1)
-    ax1.set_title("{} {}".format(shape, algorithm))
+    ax1.set_xlabel('lag [in sweeps or clusterflips]')
+    ax1.set_ylabel('C (lag)')
+
+    if xlim:
+        ax1.set_xlim(*xlim)
+
+    ax1.set_ylim(-0.1, 1)
+
+    ax1.set_title("Time series data: {} \n{} {}".format(h5path, shape_as_LxL_string, algorithm))
+
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm._A = []
     cbbar_handle = plt.colorbar(sm)
     cbbar_handle.set_label('Temperature')
-    plt.savefig(directory+"/"+str(name)+"_mag_autocorr.png",
-            bbox_inches='tight')
+
+    plt.savefig(targetdir + "/" + name + img_suffix + ".png",
+                bbox_inches='tight')
 
     pbar.finish()
-
-
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
-    pbar = pb.ProgressBar(widgets=drawwidget("Generating ACF Energy: "),
-                  maxval=len(sims)).start()
-    
-    for simnr, sim in enumerate(sims):
-        pbar.update(simnr)
-
-        T = sim['temperature'][0]
-        norm = colors.Normalize(vmin=1.4,vmax=3.6)
-        linecolor = cmap(norm(T))
-
-        energy = sim['energy']
-        ax1.plot(acf(energy, 500), c=linecolor)
-
-    ax1.set_xlabel('dt [in sweeps]')
-    ax1.set_ylabel('C (dt)')
-    ax1.set_xlim(0,100)
-    ax1.set_ylim(-0.1,1)
-    ax1.set_title("{} {}".format(shape, algorithm))
-    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
-    sm._A = []
-    cbbar_handle = plt.colorbar(sm)
-    cbbar_handle.set_label('Temperature')
-    plt.savefig(directory+"/"+str(name)+"_energy_autocorr.png",
-            bbox_inches='tight')
-
-    pbar.finish()
-
 
 def drawwidget(discription):
     """ Formats the progressbar. """
@@ -119,10 +161,31 @@ def drawwidget(discription):
                ' ', pb.ETA()]
     return widgets
 
+def main():
+    """
+    Create figures in the target directory args.targetdir. 
+    If it doesn't exist, it will be created.     """
+    from colors import rundark
+    rundark()
+
+    if not os.path.exists(args.targetdir):
+        os.makedirs(args.targetdir)
+
+    for hdf5file in args.filenames:
+        h5pyfile = h5py.File(hdf5file) 
+        name = get_basename(hdf5file)
+
+        make_acf_plot(h5pyfile, name, h5path='magnetization', norm=args.norm, length=args.length, xlim=args.xlim)
+        make_acf_plot(h5pyfile, name, h5path='energy', norm=args.norm, length=args.length, xlim=args.xlim)
+
     
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', metavar="HDF5 FILENAME")
+    parser.add_argument('filenames', nargs="+")
+    parser.add_argument('--targetdir', default='figures')
+    parser.add_argument('--xlim', nargs=2, metavar="xbegin xend",type=int)
+    parser.add_argument('--norm', nargs=2, metavar="vmin vmax", type=float)
+    parser.add_argument('--length', default=500, type=int)
     args = parser.parse_args()
     return args
 
