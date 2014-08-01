5f36143e1d783dfaf17ada8ce2b38c2bc675869a
iambernie <iambernie@gmail.com>
2014-08-01 16:13:47 +0200

    some PEP 8

--- autocorrelation.py
+++ autocorrelation.py
@@ -3,7 +3,6 @@
 import os
 import h5py
 import numpy as np
-import numpy 
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
@@ -22,20 +21,20 @@ def get_basename(filepath):
     name = os.path.splitext(basename)[0]
     return name
 
-def acf(x, length=None):
+def acf(ndarr, length=None):
     """
-    Return an ndarray with normalized correlation coefficients corresponding 
-    with lag elements given by: range(1, length).  
+    Return an ndarray with normalized correlation coefficients corresponding
+    with lag elements given by: range(1, length).
     The range starts with 1 because the correlation for lag=0 is infinity.
 
     Parameters
     ----------
-    x : ndarray of shape (1,)
-        The signal (typically time-series data) for which the autocorrelation 
+    ndarr : ndarray of shape (1,)
+        The signal (typically time-series data) for which the autocorrelation
         function C(lag) needs to calculated.
 
-    length : int 
-        End of the interval range(1, length). The normalized correlation 
+    length : int
+        End of the interval range(1, length). The normalized correlation
         coefficients are calculated for this range and returned as an ndarray.
         #TODO: or optional array of lag elements [dt1, dt2, dt3,...]
 
@@ -43,24 +42,25 @@ def acf(x, length=None):
 
     https://en.wikipedia.org/wiki/Pearson_product-moment_correlation_coefficient
 
-    for the definition of the correlation matrix. 
+    for the definition of the correlation matrix.
 
     The correlation coefficients are returned by np.corrcoef. See for details:
     http://docs.scipy.org/doc/numpy/reference/generated/numpy.corrcoef.html
-    
-    """
-    if not length: 
-        length = len(x) - 1
 
-    coeffs = [np.corrcoef(x[:-dt], x[dt:])[0,1] for dt in range(1,length)]
-    result = numpy.array([1]+ coeffs)
+    """
+    if not length:
+        length = len(ndarr) - 1
+
+    coeffs = [np.corrcoef(ndarr[:-dt], ndarr[dt:])[0, 1] \
+              for dt in range(1, length)]
+    result = np.array([1]+ coeffs)
     return result
 
 
 def make_acf_plot(h5pyfile, name, **kwargs):
     """
     Valid keyword arguments are:
-    h5path: 
+    h5path:
     norminterval:
     cmap:
     length:
@@ -68,11 +68,9 @@ def make_acf_plot(h5pyfile, name, **kwargs):
     img_suffix:
     targetdir:
     """
-    #print("Keyword arguments in make_acf_plot:", kwargs)
 
     if 'h5path' not in kwargs:
-        print("Please specify timeseries data")
-        return 0
+        raise Exception("Please specify timeseries data")
     else:
         h5path = kwargs['h5path']
 
@@ -88,9 +86,9 @@ def make_acf_plot(h5pyfile, name, **kwargs):
         cmap = cm.hot
 
     if 'length' in kwargs:
-        length = kwargs['length']  
+        length = kwargs['length']
     else:
-        length = None 
+        length = None
 
     if 'xlim' in kwargs:
         xlim = kwargs['xlim']
@@ -116,25 +114,28 @@ def make_acf_plot(h5pyfile, name, **kwargs):
     shape = firstsim['shape'].value
     algorithm = h5pyfile.attrs['mode']
 
-    shape_as_LxL_string = str(shape[0][0])+" x "+str(shape[0][1]) # e.g. "20 x 20"
+    shape_as_string = str(shape[0][0])+" x "+str(shape[0][1])
+    # shape_as_string = "20 x 20"
 
 
     print("\nHDF5 file: {}".format(h5pyfile.filename))
-    pbar = pb.ProgressBar(widgets=drawwidget("Generating ACF {}: ".format(h5path)),
-                  maxval=len(sims)).start()
-    
+    processs_description = "Generating ACF {}: ".format(h5path)
+    pbar = pb.ProgressBar(widgets=drawwidget(processs_description),
+                          maxval=len(sims)).start()
+
     for simnr, sim in enumerate(sims):
         pbar.update(simnr)
-
-        T = sim['temperature'][0]
-        #temperature based linecolor
-        linecolor = cmap(norm(T))
-
+        temperature = sim['temperature'][0]
         timeseriesdata = sim[h5path]
+        ax1.plot(acf(timeseriesdata, length), c=cmap(norm(temperature)))
 
-        ax1.plot(acf(timeseriesdata, length), c=linecolor)
+    if algorithm == 'metropolis':
+        ax1.set_xlabel('lag [in sweeps ]')
+    elif algorithm == 'wolff':
+        ax1.set_xlabel('lag [in clusterflips]')
+    else:
+        ax1.set_xlabel('lag [in ??]')
 
-    ax1.set_xlabel('lag [in sweeps or clusterflips]')
     ax1.set_ylabel('C (lag)')
 
     if xlim:
@@ -142,11 +143,12 @@ def make_acf_plot(h5pyfile, name, **kwargs):
 
     ax1.set_ylim(-0.1, 1)
 
-    ax1.set_title("Time series data: {} \n{} {}".format(h5path, shape_as_LxL_string, algorithm))
+    substitutions = (h5path, shape_as_string, algorithm)
+    ax1.set_title("Time series data: {} \n{} {}".format(*substitutions))
 
-    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
-    sm._A = []
-    cbbar_handle = plt.colorbar(sm)
+    scalarmap = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    scalarmap._A = [] #How to get around this?
+    cbbar_handle = plt.colorbar(scalarmap)
     cbbar_handle.set_label('Temperature')
 
     plt.savefig(targetdir + "/" + name + img_suffix + ".png",
@@ -156,44 +158,43 @@ def make_acf_plot(h5pyfile, name, **kwargs):
 
 def drawwidget(discription):
     """ Formats the progressbar. """
-    widgets = [discription.ljust(20), pb.Percentage(), ' ',
-               pb.Bar(marker='#',left='[',right=']'),
-               ' ', pb.ETA()]
-    return widgets
+    widget = [discription.ljust(20), pb.Percentage(), ' ',
+              pb.Bar(marker='#', left='[', right=']'), ' ', pb.ETA()]
+    return widget
 
 def main():
     """
-    Create figures in the target directory args.targetdir. 
-    If it doesn't exist, it will be created.     """
+    Create figures in the target directory ARGS.targetdir.
+    If it doesn't exist, it will be created.
+    """
     from colors import rundark
     rundark()
 
-    if not os.path.exists(args.targetdir):
-        os.makedirs(args.targetdir)
+    if not os.path.exists(ARGS.targetdir):
+        os.makedirs(ARGS.targetdir)
 
-    for hdf5file in args.filenames:
-        h5pyfile = h5py.File(hdf5file) 
+    for hdf5file in ARGS.filenames:
+        h5pyfile = h5py.File(hdf5file)
         name = get_basename(hdf5file)
 
-        make_acf_plot(h5pyfile, name, h5path='magnetization', norm=args.norm, length=args.length, xlim=args.xlim)
-        make_acf_plot(h5pyfile, name, h5path='energy', norm=args.norm, length=args.length, xlim=args.xlim)
+        make_acf_plot(h5pyfile, name, h5path='magnetization', norm=ARGS.norm,
+                      length=ARGS.length, xlim=ARGS.xlim)
+        make_acf_plot(h5pyfile, name, h5path='energy', norm=ARGS.norm,
+                      length=ARGS.length, xlim=ARGS.xlim)
 
-    
+
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs="+")
     parser.add_argument('--targetdir', default='figures')
-    parser.add_argument('--xlim', nargs=2, metavar="xbegin xend",type=int)
+    parser.add_argument('--xlim', nargs=2, metavar="xbegin xend", type=int)
     parser.add_argument('--norm', nargs=2, metavar="vmin vmax", type=float)
     parser.add_argument('--length', default=500, type=int)
-    args = parser.parse_args()
-    return args
+    arguments = parser.parse_args()
+    return arguments
 
 
 if __name__ == "__main__":
-    args = get_arguments()
-    print(args)
+    ARGS = get_arguments()
     main()
-
-
 
